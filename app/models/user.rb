fd4555fd68e8fbb368b7b1c1352f14ec72f44338
Mayu <okazakimayu@gmail.com>
2016-12-31 08:55:18 +0000

    update user-profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-    
-    has_secure_password
+  validates :password, presence: true
+  has_secure_password
+  validates :profile, length: { maximum: 160 }
 end
