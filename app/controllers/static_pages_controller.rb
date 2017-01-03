cbec3bb875902fb7399f2c858d1e6d9592a455cc
Mayu <okazakimayu@gmail.com>
2017-01-03 14:53:17 +0000

    image developpement

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
     if logged_in?
       @title = "Micropost Feeds"
       @micropost = current_user.microposts.build
-      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
+      @feed_items = current_user.feed_items.includes(:user).page(params[:page]).per(10).order(created_at: :desc)
     end
   end
 end
